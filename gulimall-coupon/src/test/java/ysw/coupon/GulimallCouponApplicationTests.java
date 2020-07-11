2723ad865f000c789776bd9efefcfb26d63a887f
ysw <1779001867@qq.com>
2020-07-11 14:09:49 +0800

    谷粒商城

---
+++ gulimall-coupon/src/test/java/ysw/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package ysw.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
